aca0655247ce8b24565f56bc690ac20890fa4742
Administrator <Administrator@SKXJNK9UTQ6NJM4>
2018-07-06 22:56:37 +0800

    添加了一静态类

---
+++ src/com/jing/AddNumber.java
@@ -0,0 +1,8 @@
+package com.jing;
+
+public class AddNumber {
+	
+	public static int add(int one,int two){
+		return one+two;
+	}
+}
--- src/com/jing/Main.java
+++ src/com/jing/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("hello");
+		int total = AddNumber.add(2, 3);
+		System.out.println(total);
 	}
 
 }
